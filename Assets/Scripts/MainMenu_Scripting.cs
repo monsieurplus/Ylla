654cd84ad06c7e43221fe56becb3412ee581be1b
Adrien Mas <mas.adrien@gmail.com>
2015-11-16 23:52:30 +0100

    Add Antoine scripts
    Antoine scripts adding :
    - Interactive objects
    - Main menu
    - Subtitles

---
+++ Assets/Scripts/MainMenu_Scripting.cs
@@ -0,0 +1,273 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu_Scripting : MonoBehaviour {
+
+    uint currentSubMenu = 0;
+
+    Canvas thisCanvas;
+    UnityEngine.UI.Image blackFader;
+
+    UnityEngine.UI.Image gameLogo;
+    UnityEngine.UI.Text clickText;
+
+    CanvasGroup mainMenuGroup;
+    CanvasGroup optionsMenuGroup;
+
+    UnityEngine.UI.Slider graphSlider;
+    UnityEngine.UI.Toggle subtitleToggle;
+
+    bool isFadeOutGameExit = false;
+
+    // Use this for initialization
+    void Awake () {
+
+        //Get the canvas
+        thisCanvas = GetComponent<Canvas>();
+
+        //Get the independant text elements
+        gameLogo = transform.Find("FakeTitle").gameObject.GetComponent<UnityEngine.UI.Image>();
+        clickText = transform.Find("Text_ClicktoBegin").gameObject.GetComponent<UnityEngine.UI.Text>();
+
+        //Get the Canvas groups for the different sub-menus
+        mainMenuGroup = transform.Find("Group_MainMenu").gameObject.GetComponent<CanvasGroup>();
+        mainMenuGroup.interactable = false;
+        optionsMenuGroup = transform.Find("Group_OptionsMenu").gameObject.GetComponent<CanvasGroup>();
+        optionsMenuGroup.interactable = false;
+
+        //Get the interactive elements from options screen
+        graphSlider = optionsMenuGroup.transform.Find("Graph_Quality_Slider").gameObject.GetComponent<UnityEngine.UI.Slider>();
+        graphSlider.value = QualitySettings.GetQualityLevel();
+
+        subtitleToggle = optionsMenuGroup.transform.Find("Subtitle_Toggle").gameObject.GetComponent<UnityEngine.UI.Toggle>();
+
+        //Reads subtitles preferences in "preferences" save file. 0 if no value. Checkbox is changed accordingly.
+        if ( PlayerPrefs.GetInt("subtitles", 0) == 1 ) { 
+            subtitleToggle.isOn = true;
+        }
+        else
+        {
+            subtitleToggle.isOn = false;
+        }
+
+        PlayerPrefs.SetInt("subtitles", 0);
+
+        //Setup the black fader to fill the screen
+        blackFader = transform.Find("BlackFader").gameObject.GetComponent<UnityEngine.UI.Image>();
+        blackFader.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.width);
+        blackFader.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.height);
+
+        //Start the coroutines to get this show on the road
+        StartCoroutine("ShineClicktext");
+        StartCoroutine("FadeScreenFromBlack");
+    }
+
+    //Coroutine which fades the menu in from black after the logos
+    IEnumerator FadeScreenFromBlack()
+    {
+        for (float f = 1.0f; f >= 0.0f; f -= 0.1f)
+        {
+            blackFader.canvasRenderer.SetAlpha(f);
+            yield return null;
+        }
+        blackFader.canvasRenderer.SetAlpha(0.0f);
+        blackFader.raycastTarget = false;
+    }
+
+    //Coroutine which causes the "click to begin" text to have variable luminosity
+    IEnumerator ShineClicktext()
+    {
+        //Until user clicks (left or right button) coroutine is stuck in loop that changes alpha
+        while (!Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1))
+        {
+            clickText.canvasRenderer.SetAlpha(0.8f + Mathf.Sin((Time.frameCount-20f)/40f) * 0.2f);
+            yield return null;
+        }
+
+        //After click, launches fadeout of text through other coroutine
+        currentSubMenu = 1;
+
+        clickText.canvasRenderer.SetAlpha(1.0f);
+        StartCoroutine("FadeClickTextOut");
+
+    }
+
+
+    //Coroutine which fades the "click to begin" text out
+    IEnumerator FadeClickTextOut()
+    {
+        Debug.Log("FadeClickTextOut");
+
+        for (float f = 1.0f; f >= 0.0f; f -= 0.1f)
+        {
+            clickText.canvasRenderer.SetAlpha(f);
+            yield return null;
+        }
+
+        //Once this text is faded out, remove text and display Main Menu
+        clickText.transform.SetParent(null);
+        DisplayMainMenu();
+
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    //Main Menu and transitions--------------------------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+
+    //Displays the main part of the menu (3buttons)
+    private void DisplayMainMenu()
+    {
+        Debug.Log("DisplayMainMenu");
+
+        //Launches fade-in effect
+        StartCoroutine("FadeMainButtonsIn");
+    }
+
+    //Coroutine which fades the main menu in
+    IEnumerator FadeMainButtonsIn()
+    {
+        for (float f = 0.0f; f <= 1.0f; f += 0.1f)
+        {
+            mainMenuGroup.alpha = f;
+            yield return null;
+        }
+
+        //Once this text is faded in, make the buttons clickable
+        mainMenuGroup.alpha = 1.0f;
+        mainMenuGroup.interactable = true;
+
+    }
+
+
+    //Starts the process to display options menu
+    public void DisplayOptionsMenu()
+    {
+        Debug.Log("DisplayOptionsMenu");
+
+        currentSubMenu = 2;
+        mainMenuGroup.interactable = false;
+
+        //As usual, a coroutine takes care of offering a smoother transition
+        StartCoroutine("FadeToOptionsMenu");
+    }
+
+    //Coroutine handling transition from main menu to options menu
+    IEnumerator FadeToOptionsMenu()
+    {
+        //First, fade out the main menu
+        for (float f = 1.0f; f >= 0.0f; f -= 0.1f)
+        {
+            mainMenuGroup.alpha = f;
+            yield return null;
+        }
+
+        mainMenuGroup.alpha = 0.0f;
+
+        //Second, fade in the options menu
+        for (float f = 0.0f; f <= 1.0f; f += 0.1f)
+        {
+            optionsMenuGroup.alpha = f;
+            yield return null;
+        }
+
+        optionsMenuGroup.alpha = 1.0f;
+
+        //Once the options menu is faded in, make the buttons clickable
+        optionsMenuGroup.interactable = true;
+
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    //Managing the Options Menu--------------------------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+
+    //Changes the graphic quality depending on the slider position
+    public void UpdateGraphicsQuality ()
+    {
+        QualitySettings.SetQualityLevel( Mathf.RoundToInt(graphSlider.value) , true);
+    }
+
+    //Changes the status saved for subtitles on toggle change
+    public void SaveSubtitleToggle () {
+
+        int valueToSave;
+        if (subtitleToggle.isOn) valueToSave = 1;  else valueToSave = 0;
+        PlayerPrefs.SetInt("subtitles", valueToSave);
+
+        Debug.Log("Value Saved : "+ valueToSave);
+    }
+
+
+    //Starts the process to display options menu
+    public void ReturnToMainMenu()
+    {
+        Debug.Log("ReturnToMainMenu");
+
+        currentSubMenu = 1;
+        optionsMenuGroup.interactable = false;
+
+        //As usual, a coroutine takes care of offering a smoother transition
+        StartCoroutine("FadeOutOptions");
+    }
+
+
+    //Fades the options out, then call the coroutine to fade in the menu
+    IEnumerator FadeOutOptions()
+    {
+        for (float f = 1.0f; f >= 0.0f; f -= 0.1f)
+        {
+            optionsMenuGroup.alpha = f;
+            yield return null;
+        }
+
+        optionsMenuGroup.alpha = 0.0f;
+        DisplayMainMenu();
+
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    //Exiting this menu (to start or quit game) - includes fade to black---------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+
+    //Launches process of starting the main game (button is setup to call this function through UNITY interface)
+    public void StartGame()
+    {
+        mainMenuGroup.interactable = false;
+        StartCoroutine("FadeOutOfMenu");
+    }
+
+    //Launches process of quitting the game (button is setup to call this function through UNITY interface)
+    public void QuitGame()
+    {
+        isFadeOutGameExit = true;
+
+        mainMenuGroup.interactable = false;
+        StartCoroutine("FadeOutOfMenu");
+    }
+
+    //Coroutine which fades to back and then exits the scene
+    IEnumerator FadeOutOfMenu()
+    {
+        blackFader.raycastTarget = true;
+
+        for (float f = 0.0f; f >= 0.0f; f -= 0.1f)
+        {
+            blackFader.canvasRenderer.SetAlpha(f);
+            yield return null;
+        }
+        blackFader.canvasRenderer.SetAlpha(0.0f);
+
+        yield return new WaitForSeconds(0.5f);
+
+        if (isFadeOutGameExit)
+        {
+            Application.Quit();
+        }
+        else
+        {
+            Application.LoadLevel("TestScene");
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/UI_Scripting.cs
@@ -0,0 +1,268 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI_Scripting : MonoBehaviour {
+
+    [SerializeField]private GameObject refPlayer;
+    [SerializeField]private GameObject SubtitleContainer;
+
+    Canvas thisCanvas;
+    UnityEngine.UI.Image blackFader;
+    UnityEngine.UI.Image whiteFader;
+
+    GameObject paperPieceObj;
+    UnityEngine.UI.Image paperPiece;
+    Vector2 desiredImageSize;
+
+    List<GameObject> SubtitleEntities = new List<GameObject>();
+
+    float fadeTime;
+
+    // Use this for initialization
+    void Start () {
+        thisCanvas = GetComponent<Canvas>();
+
+        //Initialize the black fader
+        blackFader = transform.Find("BlackFader").gameObject.GetComponent<UnityEngine.UI.Image>();
+        blackFader.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.width);
+        blackFader.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.height);
+        blackFader.canvasRenderer.SetAlpha(0f);
+
+        //Initialize the white fader
+        whiteFader = transform.Find("WhiteFader").gameObject.GetComponent<UnityEngine.UI.Image>();
+        whiteFader.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.width);
+        whiteFader.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.height);
+        whiteFader.canvasRenderer.SetAlpha(0f);
+
+    }
+
+    // Loads a sprite from Asset list
+    public void loadPaperDocument (string docName)
+    {
+        //For the script to work, we need "docName" to be "InteractiblePaper_(sprite asset name)"
+        //The asset needs to be into the Assets/Resources folder, or it will not work
+
+        //Cut the beginning of the string to isolate (sprite asset name). Uses "_" as the separator
+        string[] AssetName = docName.Split(new char[] { '_' });
+
+        //Create an "Image" (UI element) from the void. Fill it with the correct sprite using the string created previously
+        paperPieceObj = new GameObject();
+        paperPiece = paperPieceObj.AddComponent<UnityEngine.UI.Image>();
+
+        Debug.Log(AssetName[1]);
+
+        paperPiece.sprite = Resources.Load(AssetName[1], typeof(Sprite)) as Sprite;
+
+        //Set sprite as child of canvas interface
+        paperPiece.transform.parent = thisCanvas.transform;
+
+        //Read original sprite texture size
+        Vector2 dimensionsImage = new Vector2(paperPiece.sprite.texture.width, paperPiece.sprite.texture.height);
+
+        //Read format of the canvas
+        RectTransform canvasRect = thisCanvas.GetComponent<RectTransform>();
+        Vector2 dimensionsCanvas = new Vector2(canvasRect.rect.width, canvasRect.rect.height);
+
+        //Center the sprite in the middle of the screen
+        paperPiece.transform.position = new Vector3( dimensionsCanvas.x / 2, dimensionsCanvas.y / 2 );
+
+        //Calculate the size we want to display it (biggest dimension = 80% of canvas)
+        float resizeRatio;
+        desiredImageSize = dimensionsImage;
+
+        if (desiredImageSize.x >= desiredImageSize.y )
+        {
+            if (desiredImageSize.x > 0.95f*dimensionsCanvas.x)
+            {
+                resizeRatio = (0.95f * dimensionsCanvas.x) / desiredImageSize.x;
+                desiredImageSize *= resizeRatio;
+            }
+        }
+        else
+        {
+            if (desiredImageSize.y > 0.95f * dimensionsCanvas.y)
+            {
+                resizeRatio = (0.95f * dimensionsCanvas.y) / dimensionsImage.y;
+                desiredImageSize *= resizeRatio;
+            }
+        }
+
+        Debug.Log("Canvas Size :" + dimensionsCanvas + "- Desired Size :" + desiredImageSize );
+
+        //Disable normal FPS Controls
+        refPlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+        refPlayer.transform.GetChild(0).GetComponent<DetectInteractibles>().enabled = false;
+
+        //Launch the coroutine which will make the paper appear
+        StartCoroutine("FadePaperSpriteIn");
+
+    }
+
+
+    //Coroutine which causes the "pieces of paper" to fade in and grow over a few frames, and waits for click to make it dissapear
+    IEnumerator FadePaperSpriteIn()
+    {
+        RectTransform spriteRect = paperPiece.GetComponent<RectTransform>();
+        for (float f = 0.0f; f <= 1.0f; f += 0.1f)
+        {
+            blackFader.canvasRenderer.SetAlpha(0.7f*f);
+            spriteRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, f*desiredImageSize.x);
+            spriteRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, f*desiredImageSize.y);
+            yield return null;
+        }
+
+        //Use yield to wait until button is pressed. When it is start new coroutine to make paper disappear
+        while ( !Input.GetButtonDown("Fire1") ){ yield return null; }
+        StartCoroutine("FadePaperSpriteOut");
+
+    }
+
+    //Coroutine which causes the "pieces of paper" to fade in and grow over a few frames
+    IEnumerator FadePaperSpriteOut()
+    {
+        RectTransform spriteRect = paperPiece.GetComponent<RectTransform>();
+        for (float f = 1.0f; f >= 0.0f; f -= 0.1f)
+        {
+            blackFader.canvasRenderer.SetAlpha(0.7f * f);
+            spriteRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, f * desiredImageSize.x);
+            spriteRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, f * desiredImageSize.y);
+            yield return null;
+        }
+
+
+
+        //destroy the "paper" sprite
+        Destroy(paperPiece);
+        Destroy(paperPieceObj);
+
+        //Give control back to player once fade is over
+        refPlayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+        refPlayer.transform.GetChild(0).GetComponent<DetectInteractibles>().enabled = true;
+    }
+
+    //Requests a fade to black at a given speed (in seconds)
+    public void requestFadeToBlack( float seconds )
+    {
+        fadeTime = seconds;
+        StartCoroutine("FadeToBlack");
+    }
+
+    //Coroutine which causes a complete fade to black (used when "dying" from a fall)
+    IEnumerator FadeToBlack()
+    {
+        float f = 0.0f;
+
+        while (f < 1.0f)
+        {
+            f += Time.deltaTime/fadeTime;
+            if (f>1.0f) { f = 1.0f; }
+
+            blackFader.canvasRenderer.SetAlpha(f);
+            yield return null;
+        }
+
+    }
+
+    //Requests a white flash from a faded black screen (used when "respawning" after a fall
+    public void requestWhiteFlashFromBlack()
+    {
+        StartCoroutine("WhiteFlashFromBlack");
+    }
+
+    //Coroutine which executes the white flash from black (when "respawning"). Fixed speed
+    IEnumerator WhiteFlashFromBlack()
+    {
+        float f = 0.0f;
+
+        //Make white fader appear first
+        while (f < 1.0f)
+        {
+            f += Time.deltaTime*5;
+            if (f > 1.0f) { f = 1.0f; }
+
+            whiteFader.canvasRenderer.SetAlpha(f);
+            yield return null;
+        }
+
+        //Instantly hide black fader
+        blackFader.canvasRenderer.SetAlpha(0.0f);
+
+        //Make white fader disappear slower
+        while (f > 0.0f)
+        {
+            f -= Time.deltaTime;
+            if (f < 0.0f) { f = 0.0f; }
+
+            whiteFader.canvasRenderer.SetAlpha(f);
+            yield return null;
+        }
+    }
+
+    //Function to display a subtitle. It's just a data transfer.
+    public void PlayLineWithSubtitles( AudioSource source, AudioClip audio )
+    {
+        source.PlayOneShot(audio);
+
+        //Do the subtitles actually appear? Depends if they are enabled. Check the option
+        if ( PlayerPrefs.GetInt("subtitles") == 1 ) { 
+            GameObject NewSubtitleBox = Instantiate(SubtitleContainer) as GameObject;
+            NewSubtitleBox.transform.SetParent(thisCanvas.transform, false );
+            NewSubtitleBox.transform.position = new Vector3 (Screen.width/2 , Screen.height/6, 0);
+
+            Subtitles_Scripting scriptSub = NewSubtitleBox.GetComponent<Subtitles_Scripting>();
+
+            scriptSub.DisplaySubtitleMatchingTheSound(audio);
+
+            //Review existing array of subtitles, if there are any, order them to move up
+            for ( int i=0; i < SubtitleEntities.Count; i++ )
+            {
+                if ( SubtitleEntities[i] != null )
+                {
+                    //If there is previous subtitle, order it to move up
+                    scriptSub = SubtitleEntities[i].GetComponent<Subtitles_Scripting>();
+                    scriptSub.RequestMoveUp();
+                }
+                else
+                {
+                    //If array row is null (previous subtitle removed), remove it
+                    SubtitleEntities.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            //Push the new subtitle to end of array
+            SubtitleEntities.Add(NewSubtitleBox);
+        }
+    }
+
+
+    //--------------------------------------------------
+    //ONLY FOR TEST PURPOSES
+    //--------------------------------------------------
+    AudioSource loopSource;
+    AudioClip[] loopAudios;
+
+    public void startQuickLinesLoop(AudioSource source, AudioClip[] audios )
+    {
+        loopSource = source;
+        loopAudios = audios;
+        StartCoroutine("PlayRandomLinesQuick");
+    }
+
+    //Coroutine to play subtitled lines in quick succession. Very annoying to human ears but tests subtible system.
+    IEnumerator PlayRandomLinesQuick()
+    {
+        while (true) { 
+            int n = Random.Range(0, loopAudios.Length);
+            loopSource.clip = loopAudios[n];
+            PlayLineWithSubtitles(loopSource, loopSource.clip);
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+    //--------------------------------------------------
+    //ONLY FOR TEST PURPOSES
+    //--------------------------------------------------
+
+}
